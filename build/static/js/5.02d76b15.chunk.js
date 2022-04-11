667c1dbd45db035245562149ffe6bee3c1c89537
ChaiSomsri96 <kimdojin0815@gmail.com>
2022-04-11 22:09:32 +0700

    build check

---
+++ build/static/js/5.02d76b15.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpgigaland=this.webpackJsonpgigaland||[]).push([[5],{2001:function(n,a){},2013:function(n,a){}}]);
+//# sourceMappingURL=5.02d76b15.chunk.js.map
---
+++ build/static/js/6.63fbe0e1.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpgigaland=this.webpackJsonpgigaland||[]).push([[6],{1967:function(a,n){}}]);
+//# sourceMappingURL=6.63fbe0e1.chunk.js.map
